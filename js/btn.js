93fa14042a9a7cead34e994d1d8f0904947cd019
MintGame <58911169+MintGame@users.noreply.github.com>
2020-06-06 12:32:29 +0500

    Add files via upload

--- js/btn.js
+++ js/btn.js
@@ -1,6 +1,19 @@
 const menuBtn = document.querySelector(".m-menu");
 const menu = document.querySelector(".menu-sidebar");
 
+const otherBtn = document.querySelector(".other-link");
+const other = document.querySelector(".see-more");
+const link = document.querySelector(".menu-sidebar");
+// const otherActive = document.querySelector(".see-more" && ".active"):
+
 menuBtn.addEventListener('click', function() {
     menu.classList.toggle("active");
 });
+
+// otherBtn.addEventListener('mouseover', function() {
+//     other.classList.add("active");
+// });
+
+// link.addEventListener('mouseout', function() {
+//     other.classList.remove("active");
+// });
